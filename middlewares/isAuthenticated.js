977b2a9e78d1fcb36b00d98310c3c3b862085a18
shariyerShazan <shariyershazan1@gmail.com>
2025-09-06 23:53:19 +0600

    isAuthenticated middlewares

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken"
+
+export const isAuthenticated = async (req , res , next)=>{
+    try {
+        const token = req.cookies
+        if(!token){
+            return res.status(400).json({
+                message : "User not authenticated!" ,
+                success: false
+            })
+        }
+        const decode = await jwt.verify(token , process.env.JWT_SECRET_KEY)
+        if(!decode){
+            return res.status(400).json({
+                message : "Invalid token" ,
+                success: false
+            })
+        }
+        req.userId = decode.userId
+    } catch (error) {
+        console.log(error) 
+        return res.status(500).json({
+            message : "Internal server error" ,
+            success: false
+        })
+    }
+}

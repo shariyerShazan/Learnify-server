329be494235de3a4043fc052ad6efca936bc07ba
shariyerShazan <shariyershazan1@gmail.com>
2025-09-07 04:34:14 +0600

    multer add

--- utils/multer.js
+++ utils/multer.js
@@ -1,3 +1,6 @@
-import multer from "multer"
+import multer from "multer";
 
-const upload  = multer()
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
+
+export default upload;
